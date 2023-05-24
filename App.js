1f8abfe40cada07601d4a5a06aecd0878c038351
Elly Pirelly <47147981+EllyPirelly@users.noreply.github.com>
2023-05-24 13:03:12 +0200

    set-up, expo image picker, expo media library, react native maps

--- App.js
+++ App.js
@@ -1,11 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Button, Image, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as MediaLibrary from 'expo-media-library';
+import * as Location from 'expo-location';
+import MapView from 'react-native-maps';
 
-export default function App() {
+const App = () => {
+  const [image, setImage] = useState(null);
+  const [location, setLocation] = useState(null);
+
+  // lets user pick an image / ask for permission first
+  const pickImage = async () => {
+    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (permissions?.granted) {
+      // default to pick is Images
+      // let result = await ImagePicker.launchImageLibraryAsync();
+
+      let result = await ImagePicker.launchImageLibraryAsync({
+        // Images, Videos or All
+        mediaTypes: ImagePicker.MediaTypeOptions.Images
+      });
+
+      if (!result.canceled) setImage(result.assets[0]);
+      else setImage(null)
+    }
+  };
+
+  // lets user take an image / ask for permission first
+  // persists photos on the device storage
+  const takePhoto = async () => {
+    let permissions = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchCameraAsync();
+
+      if (!result.canceled) {
+        let mediaLibraryPermissions = await MediaLibrary.requestPermissionsAsync();
+
+        if (mediaLibraryPermissions?.granted) await MediaLibrary.saveToLibraryAsync(result.assets[0].uri);
+
+        setImage(result.assets[0]);
+      } else setImage(null)
+    }
+  }
+
+  // lets user take an image / ask for permission first
+  // const takePhoto = async () => {
+  //   let permissions = await ImagePicker.requestCameraPermissionsAsync();
+
+  //   if (permissions?.granted) {
+  //     let result = await ImagePicker.launchCameraAsync();
+
+  //     if (!result.canceled) setImage(result.assets[0]);
+  //     else setImage(null)
+  //   }
+  // };
+
+  // requests and accesses user's geo location
+  const getLocation = async () => {
+    let permissions = await Location.requestForegroundPermissionsAsync();
+
+    if (permissions?.granted) {
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    } else {
+      Alert.alert('No permission to read geo location.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title='Pick an image from the library'
+        onPress={pickImage}
+      />
+      <Button
+        title='Take a photo'
+        onPress={takePhoto}
+      />
+
+      <Button
+        title='Get my location'
+        onPress={getLocation}
+      />
+
+      {image &&
+        <Image
+          source={{ uri: image.uri }}
+          style={{ width: 200, height: 200 }}
+        />
+      }
+
+      {location &&
+        <MapView
+          style={{ width: 300, height: 200 }}
+          region={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />}
     </View>
   );
 }
@@ -13,8 +109,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    backgroundColor: '#ecf0f1',
+  }
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,60 @@
+# Communication App
+
+### What is this about?
+A very simple Communication App for mobile devices (phones) using React Native.
+<br>
+The App will provide users with the possibility give access and then access their media library (to access photos), give access and then access their device's camera (to take a photo), give access and then access their geo location data (to display it as a map).
+<br>
+This Communication App has been built as a small exercise for Achievement 5 in [Career Foundry's Full-Stack Web Development Program](https://careerfoundry.com/en/courses/become-a-web-developer/). It was the preparation before adding more or less the same features to the [Chat App](https://github.com/EllyPirelly/cf-chat-app).
+
+TODO: screenshots
+
+### Technical requirements
+The Shoppinglist App
+- must be written in React Native
+- must be developed using Expo (Expo CLI and Expo Go App) and Android Studio
+- must use Expo Image Picker
+- might use Expo Media Library
+- must use Expo Location
+- must use React Native Maps
+- must only use `App.js`
+- must not have styling
+
+### Feature requirements
+User
+- must be asked to give access to their device's media library
+- after giving access, is able to access images of their device's media library
+- must be asked to give access to their device's camera
+- after giving access, is able to take a picture, which will automatically (per default) be saved to the device's gallery
+- must be asked to give access to their geo location
+- after giving access, is able to access their geo location data and share it via displaying it as a map
+
+### Languages, Libraries, Frameworks, Tools
+- React Native
+- [Expo](https://expo.dev/)
+  - [Expo CLI](https://docs.expo.dev/get-started/installation/) and Expo Go App
+  - [Watchman](https://docs.expo.dev/get-started/installation/#requirements)
+- [Android Studio](https://developer.android.com/studio) to - when as in my case developing on MAC - emulate Android devices
+- Google Firestore/Firebase
+
+### Heads-up
+- at time of writing to use Expo CLI you need to downgrade Node to `16.19.0`
+
+### Global
+- `expo-CLI` - to create new projects and start running Expo
+
+### Dependencies
+- `expo` - to set up, develop and test React Native Apps
+- `expo-image-picker` - to ask permission and access photos/media library, take photos
+- `expo-media-libary` - to ask permission and by default save photos captured using the device's camera into the device's gallery
+- `expo-location` - to ask permission and retrieve users geo location data
+- `react`
+- `react-native` - to build apps for Android and iOS quickly with one codebase
+- `react-native-maps` - to display the retrieved geo location data in MapView
+
+### Dev Dependencies
+- `babel`
+
+### How to run this? - ToDo
+With Expo installed
+- `npm start` or `expo start`
